c5fb46cf3d78a5c462f044448af64b1bbb1f5b19
binomfx <binomfx@gmail.com>
2022-01-02 09:16:48 +0300

    Create UNISX_test_1.sol

---
+++ UNISX_test_1.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract UNISX_Token is ERC20, Ownable {
+    constructor() ERC20("UNISX_Token", "UNISX_test_1") {
+        _mint(msg.sender, 1000000000 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
